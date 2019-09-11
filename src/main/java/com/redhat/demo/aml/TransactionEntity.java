82ed1ec82ee1b5a788cb7a0dc855dcfedd7e1e2e
pamAdmin <>
2019-09-11 11:30:46 -0600

    moving from {/src/main/java/com/redhat/demo/aml_rules/Transaction.java} to {/src/main/java/com/redhat/demo/aml/TransactionEntity.java}

--- src/main/java/com/redhat/demo/aml/TransactionEntity.java
+++ src/main/java/com/redhat/demo/aml/TransactionEntity.java
@@ -1,33 +1,13 @@
-package com.redhat.demo.aml.model;
+package com.redhat.demo.aml;
 
-import javax.persistence.*;
-
-@Entity
-@Table(name="TRANSACTIONS")
 public class TransactionEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(name="customer_id")
     private Long custId;
 
-    @Column(name="amount")
     private Double amount;
 
-    @Column(name="customer_country")
     private String customerCountry;
 
-    @Column(name="customer_industry")
     private String customerIndustry;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public Long getCustId() {
         return custId;
