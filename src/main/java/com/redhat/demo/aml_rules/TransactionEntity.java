52fe5e9765611c6cdf1c6e87b8124d4407919c0b
pamAdmin <>
2019-09-11 11:31:51 -0600

    {/src/main/java/com/redhat/demo/aml_rules/TransactionEntity.java}

--- src/main/java/com/redhat/demo/aml_rules/TransactionEntity.java
+++ src/main/java/com/redhat/demo/aml_rules/TransactionEntity.java
@@ -1,54 +1,62 @@
 package com.redhat.demo.aml_rules;
 
-public class TransactionEntity {
-    private Long custId;
+public class Transaction {
+	private Long custId;
 
-    private Double amount;
+	private Double amount;
 
-    private String customerCountry;
+	private String customerCountry;
 
-    private String customerIndustry;
+	private String customerIndustry;
 
-    public Long getCustId() {
-        return custId;
-    }
+	public Long getCustId() {
+		return custId;
+	}
 
-    public void setCustId(Long custId) {
-        this.custId = custId;
-    }
+	public void setCustId(Long custId) {
+		this.custId = custId;
+	}
 
-    public Double getAmount() {
-        return amount;
-    }
+	public Double getAmount() {
+		return amount;
+	}
 
-    public void setAmount(Double amount) {
-        this.amount = amount;
-    }
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
 
-    public String getCustomerCountry() {
-        return customerCountry;
-    }
+	public String getCustomerCountry() {
+		return customerCountry;
+	}
 
-    public void setCustomerCountry(String customerCountry) {
-        this.customerCountry = customerCountry;
-    }
+	public void setCustomerCountry(String customerCountry) {
+		this.customerCountry = customerCountry;
+	}
 
-    public String getCustomerIndustry() {
-        return customerIndustry;
-    }
+	public String getCustomerIndustry() {
+		return customerIndustry;
+	}
 
-    public void setCustomerIndustry(String customerIndustry) {
-        this.customerIndustry = customerIndustry;
-    }
+	public void setCustomerIndustry(String customerIndustry) {
+		this.customerIndustry = customerIndustry;
+	}
 
-    @Override
-    public String toString() {
-        return "TransactionEntity{" +
-                "id=" + id +
-                ", custId=" + custId +
-                ", amount=" + amount +
-                ", customerCountry='" + customerCountry + '\'' +
-                ", customerIndustry='" + customerIndustry + '\'' +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return "TransactionEntity{" + "id=" + id + ", custId=" + custId
+				+ ", amount=" + amount + ", customerCountry='"
+				+ customerCountry + '\'' + ", customerIndustry='"
+				+ customerIndustry + '\'' + '}';
+	}
+
+	public Transaction() {
+	}
+
+	public Transaction(java.lang.Long custId, java.lang.Double amount,
+			java.lang.String customerCountry, java.lang.String customerIndustry) {
+		this.custId = custId;
+		this.amount = amount;
+		this.customerCountry = customerCountry;
+		this.customerIndustry = customerIndustry;
+	}
 }
